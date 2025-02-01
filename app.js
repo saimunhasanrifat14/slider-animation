e85bc929224d87d0c37ea6c7f816b58a804564c2
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-02-01 23:59:19 +0600

    add prevus button effect

--- app.js
+++ app.js
@@ -11,10 +11,30 @@ nextButton.onclick = function(){
 prevButton.onclick = function(){
     showSlider("prev")
 }
+let unAcceptClick;
 const showSlider = (type)=>{
+    nextButton.style.pointerEvents = 'none'
+    prevButton.style.pointerEvents = 'none'
+    carousel.classList.remove("prev", "naxt")
     let items = document.querySelectorAll(".item")
     if(type === "next"){
         listHTML.appendChild(items[0]); 
         carousel.classList.add("next")
+    }else{
+        let positionLast = items.length - 1;
+        listHTML.prepend(items[positionLast])
+        carousel.classList.add('prev')
     }
+    
+    clearTimeout(unAcceptClick);
+    unAcceptClick = setTimeout(() => {
+        nextButton.style.pointerEvents = 'auto'
+        prevButton.style.pointerEvents = 'auto'
+    }, 1500);
 }
+
+seeMoreButton.forEach(button =>{
+    button.onclick = function(){
+        carousel.classList.add('showDetails')
+    }
+})
